cfa306279ee74dd1cb8abb7943489d257a4dc0cb
yu han <yuhancv@outlook.com>
2023-06-18 22:49:08 +0800

    Initial commit

---
+++ src/test/java/com/example/kob/KobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
